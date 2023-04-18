f3395c548c3b9e82e1a787b9306b4cbb7de86a20
Shubhankar Gupta <115340551+Shubhankargupta691@users.noreply.github.com>
2023-04-18 21:40:56 +0530

    Update Random PassWord Generator.py

--- Random PassWord Generator.py
+++ Random PassWord Generator.py
@@ -1,17 +1,23 @@
 import random
+import string
 
-while 1:
+while True:
     print(" Press 1. To    Press 0. to Exit")
     user = int(input("Enter your choice: "))
     if user == 1:
         passwordlen = int(input("enter the length of your password"))
-        s = ("asdfghjklqwertyuiopzxcvbnm!@#$%^&*()_-=+/*1234567890("")<{}['']?\|;:ASDFGHJKLZXCVBNMQWERTYUIOP<>")
-        p = "".join(random.sample(s,passwordlen))
-        print("Your RANDOMLY GENERATED PASSWORD is:",p)
+
+        # s = ("asdfghjklqwertyuiopzxcvbnm!@#$%^&*()_-=+/*1234567890("")<{}['']?\|;:ASDFGHJKLZXCVBNMQWERTYUIOP<>")
+        # Define the character set to use in the password
+        s = string.ascii_letters + string.digits + string.punctuation
+
+        # password
+        p = "".join(random.sample(s, passwordlen))
+        print("Your RANDOMLY GENERATED PASSWORD is: ", p)
     elif user == 0:
         print("YOU ARE EXIT")
         break
-        
+
     else:
         print("Invalid Choice")
         break
